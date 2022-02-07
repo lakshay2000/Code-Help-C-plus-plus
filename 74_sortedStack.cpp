95bc7882b0a862d6fc6bb8cb84b00122c90ee05d
lakshay2000 <bangalakshay2000@gmail.com>
2022-02-08 02:38:21 +0530

    Stack-Questions

---
+++ 74_sortedStack.cpp
@@ -0,0 +1,29 @@
+void sortedInsert(stack<int> &s, int num)
+{
+    // base case
+    if (s.empty() || (!s.empty() && s.top() < num))
+    {
+        s.push(num);
+        return;
+    }
+    int n = s.top();
+    s.pop();
+
+    sortedInsert(s, num);
+    s.push(n);
+}
+
+void sortStack(stack<int> &stack)
+{
+    // Write your code here
+    if (stack.empty())
+    {
+        return;
+    }
+    int num = stack.top();
+    stack.pop();
+
+    sortStack(stack);
+
+    sortedInsert(stack, num);
+}
