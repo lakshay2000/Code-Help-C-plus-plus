f0475c4a3e9e8a495773aa84cc7c73da0bf59c05
lakshay2000 <bangalakshay2000@gmail.com>
2022-05-25 02:12:27 +0530

    Stacks

--- 76_MinimumCostToMakeaStringValid.cpp
+++ 76_MinimumCostToMakeaStringValid.cpp
@@ -50,3 +50,57 @@ int findMinimumCost(string str)
     int ans = (a + 1) / 2 + (b + 1) / 2;
     return ans;
 }
+
+// Minimum Add to Make valid string
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int minAddToMakeValid(string str)
+    {
+
+        stack<char> s;
+
+        for (int i = 0; i < str.length(); i++)
+        {
+            char ch = str[i];
+
+            if (ch == '(')
+            {
+                s.push(ch);
+            }
+            else
+            {
+                if (!s.empty() && s.top() == '(')
+                {
+                    s.pop();
+                }
+                else
+                {
+                    s.push(ch);
+                }
+            }
+        }
+        int count = 0;
+        while (!s.empty())
+        {
+            cout << s.top() << endl;
+            count++;
+            s.pop();
+        }
+        cout << endl;
+        return count;
+    }
+};
+
+int main()
+{
+    Solution s;
+
+    string str = "()))";
+    cout << s.minAddToMakeValid(str);
+    return 0;
+}
